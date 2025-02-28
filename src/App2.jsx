51359247069e137c0f5f737fbc64474c201631d3
JailsonMiranda <jailson.comp@gmail.com>
2025-02-27 21:12:04 -0300

    initial adjustments

---
+++ src/App2.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+
+const App2 = () => {
+  return <div>App2</div>;
+};
+
+export default App2;
+
+// rafce
+
+// Arrow Function
+// const App2 = () => <h1>Olá, Mundo!</h1>;
+
+// Nomeação de componete
+// PascalCase
+
+// Nomeação de variável, função, ....
+// camelCase // Exemplo: tamanhoDaTabelaDeClientes -> a primeira em letra minúscula
+
+// export default, posso importar com qualquer nome e sem chaves
+// export "sem default", só posso importar entre chaves e com o nome exportado
+
+// Self closing tag
+// <Header></Header>
+// <Header />
+
+// Nomeação de classes em CSS
+// Metodologia BEM
+// Blocks
+// Elements
+// Modifiers
+// bloco__eleemento--modificador
---
+++ src/components/Header.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import logoSpotify from "../assets/logo/spotify-logo2.png";
+
+const Header = () => {
+  return (
+    <div className="header">
+      <img src={logoSpotify} alt="Logo do Spotify" />
+
+      <a href="/">
+        <h1>Spotify</h1>
+      </a>
+      
+    </div>
+  );
+};
+
+export default Header;
